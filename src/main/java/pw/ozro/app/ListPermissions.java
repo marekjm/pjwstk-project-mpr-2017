bacd5dff272c76264b1696e0f48bd0f8efcb7666
Marek Marecki <marekjm@ozro.pw>
2017-01-06 18:58:34 +0100

    List programs

---
+++ src/main/java/pw/ozro/app/ListPermissions.java
@@ -0,0 +1,36 @@
+package pw.ozro.app;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ListPermissions {
+    public static void main(String[] args) throws Exception, SQLException {
+        System.out.println( "Hello World!" );
+
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver" );
+        } catch (Exception e) {
+            System.err.println("ERROR: failed to load HSQLDB JDBC driver.");
+            e.printStackTrace();
+            return;
+        }
+
+        Connection c;
+        try {
+            c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/xdb", "SA", "");
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        PermissionRepository repo = new PermissionRepository(c);
+        System.out.println(repo.count());
+
+        for (Permission each : repo.all()) {
+            System.out.println(each);
+        }
+    }
+}
---
+++ src/main/java/pw/ozro/app/ListRoles.java
@@ -0,0 +1,36 @@
+package pw.ozro.app;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ListRoles {
+    public static void main(String[] args) throws Exception, SQLException {
+        System.out.println( "Hello World!" );
+
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver" );
+        } catch (Exception e) {
+            System.err.println("ERROR: failed to load HSQLDB JDBC driver.");
+            e.printStackTrace();
+            return;
+        }
+
+        Connection c;
+        try {
+            c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/xdb", "SA", "");
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        RoleRepository repo = new RoleRepository(c);
+        System.out.println(repo.count());
+
+        for (Role each : repo.all()) {
+            System.out.println(each);
+        }
+    }
+}
