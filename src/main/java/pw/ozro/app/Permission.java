22a68692f1eb7cc3c1d85e7c365fbae7f403e587
Marek Marecki <marekjm@ozro.pw>
2017-01-06 15:49:47 +0100

    Redo permissions

--- src/main/java/pw/ozro/app/Permission.java
+++ src/main/java/pw/ozro/app/Permission.java
@@ -5,13 +5,15 @@ import pw.ozro.app.Entity;
 
 
 public class Permission extends Entity {
-    public String name;
+    public int role_id;
+    public int permission_id;
 
-    Permission(String n) {
-        name = n;
+    Permission(int rid, int pid) {
+        role_id = rid;
+        permission_id = pid;
     }
 
     public String toString() {
-        return name;
+        return (role_id + "." + permission_id);
     }
 }
